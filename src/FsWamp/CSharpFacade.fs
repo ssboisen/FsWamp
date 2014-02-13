610660d52fed0c755e17001cbd07059c919f68ac
Simon Skov Boisen <simon@skov-boisen.dk>
2014-02-13 09:32:33 +0100

    Refactored clients receiveLoop out of CSharpFacade

--- src/FsWamp/CSharpFacade.fs
+++ src/FsWamp/CSharpFacade.fs
@@ -13,32 +13,10 @@ type Client(host : string, port : int) =
     let wsc = new ClientWebSocket();
     let callIdMap = atom Map.empty<string,TaskCompletionSource<string>>
 
-    let rec reciveLoop() =
-            async {
-                if cts.Token.IsCancellationRequested then return ()
-                try
-                    let! (data, mt) = recv wsc cts.Token
-                    
-                    if mt = WebSocketMessageType.Text then
-                        let s = System.Text.UTF8Encoding.UTF8.GetString(data);
-                        match s with 
-                            | WELCOME (sessionId, serverIdent) ->
-                                return! reciveLoop()
-                            | CALLRESULT (callId, result) ->
-                                !callIdMap |> Map.tryFind callId |> function Some(tcs) -> tcs.SetResult(result) | None -> ()
-                                return! reciveLoop()
-                            | CALLERROR (callId, errorUri, errorDesc, errorDetails) ->
-                                !callIdMap |> Map.tryFind callId |> function Some(tcs) -> tcs.SetException(new InvalidWampRpcCallException(callId, errorUri, errorDesc, errorDetails)) | None -> ()
-                                return! reciveLoop()
-                            | _ -> printfn "Got unknown message"
-                with
-                    | :? OperationCanceledException -> printfn "Cancellation requested"; return()
-            }
-
     member this.Connect() =
         async {
             do! wsc.ConnectAsync(new Uri(sprintf "ws://%s:%i" host port), cts.Token) |> awaitTask
-            reciveLoop() |> Async.Start
+            reciveLoop wsc callIdMap cts.Token |> Async.Start
         } |> Async.StartAsTask :> Task
 
     member this.Disconnect() =
--- src/FsWamp/Client.fs
+++ src/FsWamp/Client.fs
@@ -5,15 +5,13 @@ open System.Threading.Tasks
 open System.Threading
 open FsWamp.Messages
 open FsWamp.Common
+open StateManagement
 
-let client host port ct =
-    async {
-        use wsc = new ClientWebSocket();
-        do! wsc.ConnectAsync(new Uri(sprintf "ws://%s:%i" host port), ct) |> awaitTask
-        let rec reciveLoop() =
+type InflightRpcCalls = Map<string,TaskCompletionSource<string>> atom
+
+let rec reciveLoop wsc (callIdMap : InflightRpcCalls) (ct : CancellationToken) =
             async {
                 if ct.IsCancellationRequested then return ()
-
                 try
                     let! (data, mt) = recv wsc ct
                     
@@ -21,19 +19,14 @@ let client host port ct =
                         let s = System.Text.UTF8Encoding.UTF8.GetString(data);
                         match s with 
                             | WELCOME (sessionId, serverIdent) ->
-                                printfn "Got welcomemessage for session: %s from server %s" sessionId serverIdent
-                                let call = new ArraySegment<_>(System.Text.UTF8Encoding.UTF8.GetBytes((sprintf "[2,%s,%s,%s]" (Guid.NewGuid().ToString("n")) "addd" "8,6" )))
-                                do! wsc.SendAsync(call, WebSocketMessageType.Text, true, ct) |> awaitTask
-                                return! reciveLoop()
+                                return! reciveLoop wsc callIdMap ct
                             | CALLRESULT (callId, result) ->
-                                printfn "Got result for callId: %s, it was: %s" callId result
-                                return! reciveLoop()
+                                !callIdMap |> Map.tryFind callId |> function Some(tcs) -> tcs.SetResult(result) | None -> ()
+                                return! reciveLoop wsc callIdMap ct
                             | CALLERROR (callId, errorUri, errorDesc, errorDetails) ->
-                                printfn "Got error: %s with description: %s for callId: %s" errorUri errorDesc callId
-                                return! reciveLoop()
+                                !callIdMap |> Map.tryFind callId |> function Some(tcs) -> tcs.SetException(new InvalidWampRpcCallException(callId, errorUri, errorDesc, errorDetails)) | None -> ()
+                                return! reciveLoop wsc callIdMap ct
                             | _ -> printfn "Got unknown message"
                 with
                     | :? OperationCanceledException -> printfn "Cancellation requested"; return()
             }
-        return! reciveLoop()
-    }
